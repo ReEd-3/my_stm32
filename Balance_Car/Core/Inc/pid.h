a87366301b49c7d8f9edcb49e269091d168ccefc
lw3 <2046859760@qq.com>
2026-02-22 13:46:50 +0800

    buildnn创建项目

---
+++ Balance_Car/Core/Inc/pid.h
@@ -0,0 +1,10 @@
+#ifndef BALANCE_CAR_PID_H
+#define BALANCE_CAR_PID_H
+
+#include "stdint.h"
+typedef struct
+{
+    uint8_t Target;
+}PID_Type;
+
+#endif //BALANCE_CAR_PID_H
---
+++ Balance_Car/Core/Src/pid.c
@@ -0,0 +1,6 @@
+//
+// Created by 梁子安 on 2026/2/22.
+//
+
+#include "pid.h"
+
